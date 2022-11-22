b8f082211526982de46b6ad76e931b9ac8c21bbc
Eiji Tsuji <>
2022-11-23 08:29:49 +0900

    問い合わせフォーム実装

--- pages/confirm.jsx
+++ pages/confirm.jsx
@@ -1,27 +1,50 @@
 import { useRouter } from "next/router";
 import Meta from '../compornents/meta';
+import { useState } from 'react';
 
 export default function Confirm() {
+
+  const router = useRouter();
+  const [name, setName] = useState(router.query.name);
+  const [email, setEmail] = useState(router.query.email);
+  const [subject, setSubject] = useState(router.query.subject);
+  const [content, setContent] = useState(router.query.content);
+
   return (
     <>
       <Meta title='Confirm'/>
       <div id='wrapper' className='mail-wrapper'>
       <h3>入力内容をご確認ください。</h3>
-        <div className='mail-form'>
-          <form action="">
+        <div className='confirm-form'>
+          <form method="post" action="https://docs.google.com/forms/u/0/d/e/1FAIpQLSfhjilkBxETk3oI4FkSlW9-f6dw2OzqrmgqZXspe5k4bJ8Bkg/formResponse" className="autoConfirm">
             <ul>
               <li>お名前</li>
-              <li><input type="text" /></li>
+              <li>
+                <span>{name}</span>
+              </li>
               <li>メールアドレス</li>
-              <li><input type="text" /></li>
+              <li>
+                <span>{email}</span>
+              </li>
               <li>件名</li>
-              <li><input type="text" /></li>
+              <li>
+                <span>{subject}</span>
+              </li>
               <li>内容</li>
-              <li><textarea name="" id="" cols="30" rows="10"></textarea></li>
+              <li>
+                <span>{content}</span>
+              </li>
             </ul>
-            <button className='btn'>送信</button>
+            <input type="hidden" name="entry.2005620554" value={name} />
+            <input type="hidden" name="entry.469344052" value={email} />
+            <input type="hidden" name="entry.1166974658" value={subject} />
+            <input type="hidden" name="entry.839337160" value={content} />
+            <div className="confirm-btn-line">
+              <button id="confirm-submit" className='btn'>送信</button>
+              <button id="confirm-back" className='btn'>戻る</button>
+            </div>
           </form>
-          <div className='mail-empty'></div>
+          <div className='confirm-empty'></div>
         </div>
       </div>
     </>
--- pages/mail.jsx
+++ pages/mail.jsx
@@ -1,13 +1,28 @@
 import Meta from '../compornents/meta';
 import Link from 'next/link';
 import { useState } from 'react';
+import { useRouter } from "next/router";
 
 export default function Mail() {
 
+  const router = useRouter();
   const [name, setName] = useState('');
-  const [mail, setMail] = useState('');
+  const [email, setEmail] = useState('');
   const [subject, setSubject] = useState('');
   const [content, setContent] = useState('');
+
+  const submit = async (event) => {
+    event.preventDefault();
+    router.push({
+      pathname: "/confirm",
+      query: { 
+        name: name ,
+        email: email ,
+        subject: subject ,
+        content: content
+      }
+    });
+  }
 
   return (
     <>
@@ -15,20 +30,20 @@ export default function Mail() {
       <div id='wrapper' className='mail-wrapper'>
       <h3>各種お問い合わせ等は以下のメールフォームをご利用ください。</h3>
         <div className='mail-form'>
-          <form action="">
+          <form onSubmit={submit}>
             <ul>
               <li>お名前</li>
-              <li><input type="text" /></li>
+              <li><input type="text" value={name} onChange={(e) => setName(e.target.value)} /></li>
               <li>メールアドレス</li>
-              <li><input type="text" /></li>
+              <li><input type="email" value={email} onChange={(e) => setEmail(e.target.value)} /></li>
               <li>件名</li>
-              <li><input type="text" /></li>
+              <li><input type="text" value={subject} onChange={(e) => setSubject(e.target.value)}  /></li>
               <li>内容</li>
-              <li><textarea name="" id="" cols="30" rows="10"></textarea></li>
+              <li>
+                <textarea cols="30" rows="10" value={content} onChange={(e) => setContent(e.target.value)} />
+              </li>
             </ul>
-            <Link href="/confirm">
-              <button className='btn'>送信</button>
-            </Link>
+            <button id='submit-btn' type="submit" className='btn'>送信</button>
           </form>
           <div className='mail-empty'></div>
         </div>
