34b2a64d6ae27c8d46c72c156ded043fbf6e951d
Eiji Tsuji <>
2022-12-06 00:47:07 +0900

    Googleアナリティクス搭載

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,10 +2,43 @@ import '../styles/globals.css'
 import '../styles/reset.css'
 import '../styles/style.css'
 import Layout from '../compornents/layout'
+import Script from "next/script";
+import * as gtag from "../libs/gtag";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
+
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouterChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouterChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouterChange);
+    };
+  }, [router.events]);
+
   return (
     <Layout>
+      <Script
+         strategy="afterInteractive"
+         src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_MEASUREMENT_ID}`}
+       />
+       <Script
+         id="gtag-init"
+         strategy="afterInteractive"
+         dangerouslySetInnerHTML={{
+           __html: `
+           window.dataLayer = window.dataLayer || [];
+           function gtag(){dataLayer.push(arguments);}
+           gtag('js', new Date());
+ 
+           gtag('config', '${gtag.GA_MEASUREMENT_ID}');
+           `,
+         }}
+       />
       <Component {...pageProps} />
     </Layout>
   )
