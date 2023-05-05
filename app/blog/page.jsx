d1adf9cb002b0a16860a870a845259a04ced0c34
Eiji Tsuji <>
2023-05-05 09:33:16 +0900

    yy

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -11,26 +11,23 @@ export default async function Blog() {
     return <h2>No contents</h2>;
   }
 
-  console.log(contents);
   return (
     <div className="my-4">
       <ul className="grid grid-cols-1 lg:grid-cols-4 gap-8">
         {contents.map((blog) => {
-          const img = "/img/blog/" + blog.category + ".png";
+          const img = "/img/blog/" + blog.category_2 + ".png";
+          console.log(img);
           return (
             <li key={blog.id} className="border border-gray-400 mx-10">
               <Image
                 src={img}
-                alt={blog.category}
+                alt={blog.category_2}
                 width={300}
                 height={300}
                 className="m-auto w-full h-auto"
               />
               <Link className="block" href={`/blog/${blog.id}`}>
-                <div className="flex justify-between mx-8">
-                  <p>{blog.category}</p>
-                  <p>{format(parseISO(blog.publishedAt), 'yyyy/MM/dd', {locale:ja})}</p>
-                </div>
+                <p>{format(parseISO(blog.publishedAt), 'yyyy/MM/dd', {locale:ja})}</p>
                 <p className="block p-2">{blog.title}</p>
               </Link>
             </li>
