268840015fc0f35e8c876e889b01a76285ff37ff
Eiji Tsuji <>
2023-09-08 00:26:41 +0900

    不要なコンポーネントを削除

--- app/layout.jsx
+++ app/layout.jsx
@@ -20,9 +20,6 @@ export default function RootLayout({ children }) {
           {children}
         </div>
         <Footer />
-        {/* <footer className="text-center bg-gray-800 text-gray-100 py-4">
-          <h5>© Tsuji Eiji 2023</h5>
-        </footer> */}
       </body>
     </html>
   );
